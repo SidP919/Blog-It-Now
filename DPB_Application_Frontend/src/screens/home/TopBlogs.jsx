fabedefadeafb971daf0a7998d4b803614d33b0b
sidp919 <sidp0008@gmail.com>
2024-01-28 02:45:37 +0530

    Frontend-change: made changes in TopBlogs component to utilize newly created useFetch-hook, TitleView and updated CustomCarousel

--- DPB_Application_Frontend/src/screens/home/TopBlogs.jsx
+++ DPB_Application_Frontend/src/screens/home/TopBlogs.jsx
@@ -1,163 +1,33 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-import useCommonParams from '../../hooks/useCommonParams';
-import {postAuthScreenStyle} from '../../utils/commonStyles';
 import CustomCarousel from '../../components/CustomCarousel';
 import BlogCard from '../../components/BlogCard';
-import {
-  ifMobileDevice,
-  ifTablet,
-  ifWebSmallLandscapeMode,
-  isMobileNative,
-  logger,
-} from '../../utils/utils';
-import webService from '../../services/web-service';
-import {DATA_REFRESH_DELAY, GET_TOP_BLOGS} from '../../utils/constants';
-import {NO_DATA_MSG, TOP_BLOGS_TITLE} from '../../utils/content';
+import {GET_TOP_BLOGS} from '../../utils/constants';
+import {TOP_BLOGS_TITLE} from '../../utils/content';
 import {
   getTopBlogsData,
   setTopBlogsData,
 } from '../../redux/slices/BlogsDataSlice';
+import TitleView from '../../components/TitleView';
+import useFetch from '../../hooks/useFetch';
 
 const TopBlogs = () => {
   const topBlogs = useSelector(getTopBlogsData);
   const dispatch = useDispatch();
-  const {
-    screenHeight,
-    screenWidth,
-    theme,
-    isLandscapeMode,
-    isLoggedIn,
-    Colors,
-    bigSize,
-    mdSize,
-    smSize,
-    mdText,
-    smText,
-  } = useCommonParams();
-
-  const commonStyles = postAuthScreenStyle(
-    screenHeight,
-    screenWidth,
-    theme,
-    isLandscapeMode,
-    Colors,
-    bigSize,
-    mdSize,
-    smSize,
-    mdText,
-    smText,
-  );
-
-  const fetchTopBlogs = useCallback(async () => {
-    const topBlogRes = await webService
-      .getData(GET_TOP_BLOGS)
-      .then(res => {
-        return res.data.topBlogs;
-      })
-      .catch(error => logger(error));
-    if (Array.isArray(topBlogRes)) {
-      dispatch(setTopBlogsData(topBlogRes));
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const {data} = useFetch(GET_TOP_BLOGS);
 
   useEffect(() => {
-    if (Array.isArray(topBlogs) && topBlogs.length < 1) {
-      fetchTopBlogs();
+    if (data && data.topBlogs && Array.isArray(data.topBlogs)) {
+      dispatch(setTopBlogsData(data.topBlogs));
     }
-    const topBlogsInterval = setInterval(fetchTopBlogs, DATA_REFRESH_DELAY);
-    return () => {
-      clearInterval(topBlogsInterval);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const styles = style(
-    screenHeight,
-    screenWidth,
-    theme,
-    isLandscapeMode,
-    Colors,
-    bigSize,
-    mdSize,
-    smSize,
-    mdText,
-    smText,
-  );
+  }, [data, dispatch]);
 
   return (
     <>
-      <View style={[styles.sectionTitleView]}>
-        <Text style={[commonStyles.sectionTitle, styles.sectionTitle]}>
-          {TOP_BLOGS_TITLE}
-        </Text>
-      </View>
-      <View style={[styles.carouselView]} accessible={false}>
-        {topBlogs?.length > 0 ? (
-          <CustomCarousel data={topBlogs} RenderItem={BlogCard} />
-        ) : (
-          <View style={[styles.noDataView]}>
-            <Text style={[commonStyles.dataTitle, styles.noDataText]}>
-              {NO_DATA_MSG}
-            </Text>
-          </View>
-        )}
-      </View>
+      <TitleView title={TOP_BLOGS_TITLE} />
+      <CustomCarousel data={topBlogs} RenderItem={BlogCard} />
     </>
   );
 };
 
 export default TopBlogs;
-
-const style = (
-  screenHeight,
-  screenWidth,
-  theme,
-  isLandscapeMode,
-  Colors,
-  bigSize,
-  mdSize,
-  smSize,
-  mdText,
-  smText,
-) =>
-  StyleSheet.create({
-    sectionTitleView: {
-      height:
-        (isLandscapeMode && !ifMobileDevice()) || ifTablet() || isMobileNative
-          ? 80
-          : !isLandscapeMode
-          ? 60
-          : 48,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    sectionTitle: {
-      textAlign: 'center',
-      textDecorationLine: 'none',
-    },
-    carouselView: {
-      height:
-        screenHeight -
-        ((isLandscapeMode && !ifMobileDevice()) || ifTablet() || isMobileNative
-          ? 80
-          : !isLandscapeMode
-          ? 60
-          : 48) -
-        (ifMobileDevice() || ifTablet() ? 56 : 85),
-      maxHeight: 556,
-      justifyContent: 'flex-start',
-      paddingTop: 0,
-      alignItems: 'center',
-    },
-    noDataView: {
-      height: '80%',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    noDataText: {
-      textAlign: 'center',
-    },
-  });
